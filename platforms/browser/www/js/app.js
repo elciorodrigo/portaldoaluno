2827888116434da8ed798a815aa811905dd668ea
elciorodrigo <elciorodrigo@gmail.com>
2016-12-14 03:47:40 -0200

    ok

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -4,77 +4,21 @@ module.controller("appController", [ '$scope', 'Servico', function ($scope, Serv
 
   
     $scope.exibir = "login";
-
-    $scope.alunoDisciplina1 = {
-    	disciplina : {
-    		descricao : 'Engenharia de Software'
-    	},
-    	faltas : 2,
-    	nota1 : 2.5,
-    	nota2 : 7.5
-
-    };
-
-    $scope.alunoDisciplina2 = {
-    	disciplina : {
-    		descricao : 'Matemática'
-    	},
-    	faltas : 10,
-    	nota1 : 8,
-    	nota2 : 7
-
-
-    };
-
-    $scope.alunoDisciplina3 = {
-    	disciplina : {
-    		descricao : 'Português'
-    	},
-    	faltas : 15,
-    	nota1 : 5,
-    	nota2 : 3
-
-
-    };
-    $scope.alunoDisciplina4 = {
-    	disciplina : {
-    		descricao : 'Banco de Dados'
-    	},
-    	faltas : 0,
-    	nota1 : 10,
-    	nota2 : 7
-
-
-    };
-     $scope.alunoDisciplina5 = {
-    	disciplina : {
-    		descricao : 'Redes'
-    	},
-    	faltas : 8,
-    	nota1 : 5,
-    	nota2 : 9
-
-
-    };
-
-
-
     $scope.alunosDisciplinas = [];
-    $scope.alunosDisciplinas.push( $scope.alunoDisciplina1 );
-    $scope.alunosDisciplinas.push( $scope.alunoDisciplina2 );
-      $scope.alunosDisciplinas.push( $scope.alunoDisciplina3 );
-        $scope.alunosDisciplinas.push( $scope.alunoDisciplina4 );
-          $scope.alunosDisciplinas.push( $scope.alunoDisciplina5 );
+     $scope.aluno = {
+    	codigo : '',
+    	senha: ''
+    };
+   
 
+    $scope.acessar = function (aluno) {   
 
-    $scope.professor = {
-        email : '',
-        senha : ''
-      };
+    	Servico.acessar(aluno).success(function (data){
+           $scope.alunosDisciplinas = data;
+          }).error(function(){
+            alert('erro');
+          });
 
-    $scope.acessar = function () {   
-
-          $scope.exibir = "notas";
          
                                   
     };
--- platforms/browser/www/js/services.js
+++ platforms/browser/www/js/services.js
@@ -2,47 +2,13 @@ angular.module('app').factory('Servico', [ '$http', function ($http) {
   	
     return {
   		
-      logar: function (login) {
-  			return $http({
-  				method: 'GET',
-  				url: 'https://damp-crag-8899.herokuapp.com/dadosCadastrais',
-  				params: login
-        });
-      },
-
-
-
-      salvarAluno: function (aluno) {
-        return $http({
-          method: 'POST',
-          url: 'http://127.0.0.1:8080/aluno/salvar',
-          params: aluno
-        });
-      },
-
-      editarAluno: function (aluno) {
-        return $http({
-          method: 'POST',
-          url: 'http://127.0.0.1:8080/aluno/editar',
-          params: aluno
-        });
-      },
-
-      excluirAluno: function (aluno) {
-        return $http({
-          method: 'POST',
-          url: 'http://127.0.0.1:8080/aluno/excluir',
-          params: aluno
-        });
-      },
-
-      listarAlunos: function (aluno) {
+      acessar: function (obj) {
         return $http({
           method: 'GET',
-          url: 'http://127.0.0.1:8080/aluno/listar',
-          params: aluno
+          url: 'http://192.168.1.100:8080/aluno/acessar',
+          params:obj
         });
-      }      
+      }    
     
     };
 }]);

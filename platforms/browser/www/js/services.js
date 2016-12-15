fe5f1fb7936bda5fb2402a0ef79187992c1838b0
elciorodrigo <elciorodrigo@gmail.com>
2016-12-15 15:08:46 -0200

    não era pra fazer isso agora

--- platforms/browser/www/js/services.js
+++ platforms/browser/www/js/services.js
@@ -6,7 +6,8 @@ angular.module('app').factory('Servico', [ '$http', function ($http) {
         return $http({
           method: 'GET',
          // url: 'http://127.0.0.1:8080/aluno/acessar',
-          url: 'http://192.168.0.102:8080/aluno/acessar',
+         // url: 'http://192.168.0.102:8080/aluno/acessar',
+          url: 'http://192.168.43.100:8080/aluno/acessar',
           params:obj
         });
       }    
